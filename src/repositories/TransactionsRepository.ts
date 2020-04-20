1c581441bb4d7f2b1dd1d09d3a59d360f7eb584d
vandermnt <vanderson.mantovani@hotmail.com>
2020-04-20 01:04:37 -0300

    back-end transiction financial

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,6 @@
 import Transaction from '../models/Transaction';
+// import BalanceModel from '../models/Balance';
+// import Balance from '../models/Balance';
 
 interface Balance {
   income: number;
@@ -6,23 +8,51 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactioDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balanco!: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balanco = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    this.balanco.total = this.balanco.income - this.balanco.outcome;
+    return this.balanco;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactioDTO): Transaction {
+    if (type === 'income') {
+      this.balanco.income += value;
+      const transaction = new Transaction({ title, value, type });
+
+      this.transactions.push(transaction);
+
+      return transaction;
+    }
+
+    this.balanco.outcome += value;
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
